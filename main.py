46ce3ccab2829fd9563f984d8ee2461b46003a88
Derek Nasawang <nasawangd@gmail.com>
2020-02-09 09:50:47 -0800

    added the equation for calculation of distance.

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ GPIO.setmode(GPIO.BOARD)
 #These pins will be the ones that will be used for inputs
 #Pin numbers that are now set to variables.
 GPIO_Front_Trigger = 7
-GPIO_LeftShoulder_Trigger = 11
+GPIO_LeftShoulder_Trigger_Output = 11
 #GPIO_RightShoulder_Trigger = 27
 #GPIO_BackSide_Trigger = 22
 #be sure to change the numbers to pins names instead.
 GPIO.setup(GPIO_Front_Trigger, GPIO.IN)
-GPIO.setup(GPIO_LeftShoulder_Trigger, GPIO.OUT)
+GPIO.setup(GPIO_LeftShoulder_Trigger_Output, GPIO.OUT)
 #GPIO.setup(GPIO_RightShoulder_Trigger, GPIO.IN)
 #GPIO.setup(GPIO_BackSide_Trigger, GPIO.IN)
 #GPIO.setup(5, GPIO.IN)
@@ -23,12 +23,44 @@ def distance():
     #output function for the front trigger will set to true.
     #As long as the script is running the trigger will fire.
     #Bare minumum code so far to try and see if trigger works.
+
+    #GPIO Trigger has been set to high here
     GPIO.output(GPIO_LeftShoulder_Trigger, True)
-    print(GPIO.output(GPIO_LeftShoulder_Trigger, True))
+
+    #Set sensor to sleep in 0.01 seconds. 
+    #Reset Sensor back to off.
+    time.sleep(0.01)
+    GPIO.output(GPIO_LeftShoulder_Trigger, False)
+
+    #Set the time variables for distance
+    startTime = time.time()
+    endTime= time.time()
+
+    #need to save the start and end time. That will count as one cycle
+    #Save the initial start time
+    while GPIO.input(GPIO_LeftShoulder_Trigger_Output) == 0:
+        startTime = time.time()
+    
+    #End time will be when the signal comes back. 
+    while GPIO.input(GPIO_LeftShoulder_Trigger_Output) == 1:
+        endTime = time.time()
+    
+    #To figure out the time it takes for the signal to make one full cycle we can 
+    TimeCycle = endTime - startTime
+
+    #to calculate distance its (Time difference) * Sonic Speed (34300 cm\s / 2)
+    #divide by 2 because the signal is going to destination and then bouncing back. 
+    distance = (TimeCycle * 34300) / 2
+
+    return distance
+
+
+    #print(GPIO.output(GPIO_LeftShoulder_Trigger, True))
 
 
     return distance
 
 #This code will constantly fire, printing out the measurement from the sensor
 while True:
-    distance()
+    dist = distance()
+    print("YOOo this time is measured in" % dist)
