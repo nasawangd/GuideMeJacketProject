eca0e45f00d0b668804b5f40442503047f317c46
Nighthawk8799 <rbmyrick@uw.edu>
2020-02-18 22:18:11 -0800

    Update vibrate.py

--- vibrate.py
+++ vibrate.py
@@ -12,33 +12,33 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 # In the next line, we are setting the vibrator to pin 40 (or GPIO21)
-vibrator = 40
+front_left_vibrator = 40
 
 # In the next line, the PIN is being pulsed at 3V by setting up the pin
 # itself to output with GPIO.OUT
-GPIO.setup(vibrator, GPIO.OUT)
+GPIO.setup(front_left_vibrator, GPIO.OUT)
 
 # The following methods/definitions is setting the GPIO to HIGH or LOW, which
 # essentially powers on/off the pin, respectively. 
-def steadyPulse():
+def front_left_steadyPulse():
         GPIO.output(40, GPIO.HIGH)
 
-def stopPulsing():
+def front_left_stopPulsing():
         GPIO.output(40, GPIO.LOW)
 
 # Sending energy to the pin once the obstacle is about 12ft away (365cm)...
-def quarterPulse():
+def front_left_quarterPulse():
         # start feeding the pin 3V
-        steadyPulse()
+        front_left_steadyPulse()
         # the two following lines allow for the pin to be powered
         # for 1/4 of a second, and then finally stop.
         time.sleep(0.25)
-        stopPulsing()
+        front_left_stopPulsing()
 	time.sleep(0.25)
-def minPulse():
-	steadyPulse()
+def front_left_minPulse():
+	front_left_steadyPulse()
 	time.sleep(1)
-	stopPulsing()
+	front_left_stopPulsing()
 	time.sleep(1)
 # Executing the program, which starts the vibtrator for 5 seconds then
 # stops.
